12e701d159ffd3ce9e4e3e1ff0bc5285fe9870ed
dania-meira <dania@eyeem.com>
2017-07-17 14:26:03 +0200

    stats for news dataset of 2007, 2011 and 2013

--- data_exploration.py
+++ data_exploration.py
@@ -5,6 +5,22 @@ http://www.statmt.org/wmt14/translation-task.html
 News Crawl: articles from 2011	(EN) 784 MB  http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2011.fr.shuffled.gz
 News Crawl: articles from 2013	(EN) 1.1 GB  http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2013.en.shuffled.gz
 
+news.2007.en.clean - 3,782,549 rows.
+news.2007.en.filtered - 3,780,034 rows.
+news.2007.en.split - 13,745,293 rows.
+news.2007.en.split2 - 708,160 rows.
+news.2007.en.split3 - 245,469 rows.
+news.2011.en.clean - 15,437,675 rows.
+news.2011.en.filtered - 15,420,582 rows.
+news.2011.en.split - 54,897,272 rows.
+news.2011.en.split2 - 3,186,453 rows.
+news.2011.en.split3 - 900,391 rows.
+news.2013.en.clean - 21,688,363 rows.
+news.2013.en.filtered - 21,641,708 rows.
+news.2013.en.split - 76,415,056 rows.
+news.2013.en.split2 - 4,304,937 rows.
+news.2013.en.split3 - 1,248,269 rows.
+
 '''
 
 import os
@@ -19,8 +35,8 @@ def read_and_count(filename):
 
 if __name__ == '__main__':
 
-    year_list = [2011, 2013]
-    files_list = ["clean", "filtered", "split"]#, "split2", "split4"]
+    year_list = [2007, 2011, 2013]
+    files_list = ["clean", "filtered", "split", "split2", "split3"]
 
     for y, f in list(itertools.product(year_list, files_list)):
         NEWS_FILE_NAME = os.path.join(keras_spell.DATA_FILES_FULL_PATH, "news.{0}.en.{1}".format(y, f))
